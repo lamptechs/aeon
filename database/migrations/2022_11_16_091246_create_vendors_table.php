5870960420394863dc7b03032e74a31e3bad529c
lamptechs <abdulkaderzilani98@gmail.com>
2022-11-16 21:10:12 +0600

    creating table

---
+++ database/migrations/2022_11_16_091246_create_vendors_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVendorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vendors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('logo')->nullable();
+            $table->string('address')->nullable();
+            $table->string('email')->nullable();
+            $table->string('contact_number')->nullable();
+
+            $table->string('remarks')->nullable();
+            $table->enum('status',["Active","Inactive","Pending","Cencle","Delete"]);
+            $table->string('create_by')->nullable();
+            $table->date('create_date')->nullable();
+            $table->string('modified_by')->nullable();
+            $table->date('modified_date')->nullable();
+            $table->string('deleted_by')->nullable();
+            $table->date('deleted_date')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vendors');
+    }
+}
